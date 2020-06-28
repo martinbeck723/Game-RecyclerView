ec3bac7f82f03c2232b6931a81cf846959b6a1ea
martinbeck723 <xishuoliu514@gmail.com>
2020-06-28 10:42:27 -0400

    first commit

---
+++ app/src/main/java/com/example/gamerecyclerview/models/BlogPost.kt
@@ -0,0 +1,11 @@
+package com.example.gamerecyclerview.models
+
+class GamePost(
+    var title: String,
+    var body: String,
+    //using URL to get image, so image is a String
+    var image: String,
+    var username: String
+){
+
+}
